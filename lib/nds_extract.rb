a1e47a60ebe613854fbbcaa0f4ad293517b5f758
Erik Kasperson <chefkasperson@gmail.com>
2019-11-25 15:23:08 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -20,27 +20,27 @@ def directors_totals(nds)
   # Be sure to return the result at the end!
   # require 'pp'
   # pp directors_database
-#   count = 0
-#   counter = 0
-#   ss = 0 
-#   while counter < nds.length do
-#     name = nds[counter][:name]
-#   while count < nds[counter][:movies].length do
-#   ss += nds[counter][:movies][count][:worldwide_gross]
-#   count += 1
-# end 
-#   total = {name => ss}
-#   result = result.merge!(total)
-#   ss = 0
-#   counter += 1
-# end
-# puts result
-inner_count = 0
-gross = 0
-
-while inner_count < directors_database[0][:movies][inner_count].length do
-  gross += directors_database[0][:movies][inner_count][:worldwide_gross]
-  inner_count += 1
+  count = 0
+  counter = 0
+  ss = 0 
+  while counter < nds.length do
+    name = nds[counter][:name]
+  while count < nds[counter][:movies].length do
+  ss += nds[counter][:movies][count][:worldwide_gross]
+  count += 1
+end 
+  total = {name => ss}
+  result = result.merge!(total)
+  ss = 0
+  counter += 1
 end
-puts gross
+puts result
+# inner_count = 0
+# gross = 0
+
+# while inner_count < directors_database[0][:movies][inner_count].length do
+#   gross += directors_database[0][:movies][inner_count][:worldwide_gross]
+#   inner_count += 1
+# end
+# puts gross
 end
